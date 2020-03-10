64ffd822d3e498172198ac217e8694d9cb5f418b
Devan Hare <devanhare.blog@gmail.com>
2020-03-10 14:22:04 -0500

    Still Building

--- JS/main.js
+++ JS/main.js
@@ -1,25 +1,23 @@
-const alertBanner = getElementById("alert");
+const alertBanner = document.getElementById("alert");
 
 //create the html for the banner
-alert.innerHTML = (
-  <div class="alert-banner">
-    <p>
-      <strong>Alert:</strong>You have <strong>6</strong> overdue tasks to
-      complete
-    </p>
-    <p class="alert-banner-close">x</p>
-  </div>
-);
+alertBanner.innerHTML = `
+    <div class="alert-banner">
+      <p><strong>Alert:</strong> You have <strong>6</strong> overdue tasks
+      to complete</p>
+      <p class="alert-banner-close">x</p>
+    </div>
+  `;
 
 alertBanner.addEventListener("click", e => {
   const element = e.target;
   if (element.classList.contains("alert-banner-close")) {
-    alert.style.display = "none";
+    alertBanner.style.display = "none";
   }
 });
 
 //line graph
-const trafficCanvas = getElementById("traffic-chart");
+const trafficCanvas = document.getElementById("traffic-chart");
 
 let trafficData = {
   labels: [
